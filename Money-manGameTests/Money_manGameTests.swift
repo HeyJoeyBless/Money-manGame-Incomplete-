8d4f6207e8e1dc0999be85932ada8205e0cb2287
HeyJoeyBless <jblessman24@msu.idserve.net>
2025-01-28 10:18:48 -0500

    Initial Commit

---
+++ Money-manGameTests/Money_manGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Money_manGameTests.swift
+//  Money-manGameTests
+//
+//  Created by Joseph Allen Blessman on 1/28/25.
+//
+
+import Testing
+@testable import Money_manGame
+
+struct Money_manGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
